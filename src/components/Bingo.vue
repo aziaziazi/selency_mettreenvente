e18ce515acbae4bd2b45d5e23d9bdb07548feaa9
camille <aziaziazi@gmail.com>
2018-06-13 21:31:34 +0200

    add bingo

---
+++ src/components/Bingo.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="Container">
+        <div class="Bingo">
+        Bingo, vous avez un acheteur
+        </div>
+        <CenterLine />
+    </div>
+</template>
+
+<script>
+import CenterLine from './CenterLine'
+
+export default {
+  name: 'Bingo',
+  components: {
+    CenterLine,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Bingo {
+    display: inline-block;
+    background-color: black;
+    color: white;
+    line-height: 2em;
+    padding: 0 15px;
+    font-size: 1.3em;
+}
+
+.Container {
+  position: relative;
+  padding-bottom: 80px;
+}
+</style>
